9206316e59050b4eddd884fbde8dd1faea795ee2
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-23 09:52:46 -0500

    username and password setup

--- src/components/setup/AccountSetup.tsx
+++ src/components/setup/AccountSetup.tsx
@@ -1,13 +1,40 @@
+import type React from "react"
 import { useFormContext } from "../../hooks/useFormProvider"
+import { useNavigate } from "react-router-dom"
 
 export const AccountSetup = () => {
-    const { Form_Actions, dispatch } = useFormContext()
+    const { state, Form_Actions, dispatch } = useFormContext()
+    const navigate = useNavigate()
+
+    const handleConfirmPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value: string = e.target.value
+        // let style = e.target.style
+
+        if (value !== state.password) {
+            e.target.style = "color: red"
+        } else {
+            e.target.style = "color: black"
+        }
+    }
+
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        const confirmPassword = document.getElementById(
+            "confirm"
+        ) as HTMLInputElement
+        if (confirmPassword && confirmPassword.value === state.password) {
+            navigate("/preferences")
+        } else {
+            window.alert("Please fill out the form")
+        }
+    }
     return (
         <form action="">
             <h1>Account Setup</h1>
             <fieldset>
-                <label htmlFor="">Username</label>
+                <label htmlFor="username">Username</label>
                 <input
+                    id="username"
                     type="text"
                     onChange={e =>
                         dispatch({
@@ -18,8 +45,10 @@ export const AccountSetup = () => {
                 />
             </fieldset>
             <fieldset>
-                <label htmlFor="">Password</label>
+                <label htmlFor="password">Password</label>
                 <input
+                    disabled={state.username === ""}
+                    id="password"
                     type="password"
                     onChange={e =>
                         dispatch({
@@ -30,10 +59,15 @@ export const AccountSetup = () => {
                 />
             </fieldset>
             <fieldset>
-                <label htmlFor="">Confirm Password</label>
-                <input type="password" />
+                <label htmlFor="confirm">Confirm Password</label>
+                <input
+                    onChange={handleConfirmPassword}
+                    disabled={state.password === ""}
+                    type="password"
+                    id="confirm"
+                />
             </fieldset>
-            <button>Continue</button>
+            <button onClick={handleSubmit}>Continue</button>
         </form>
     )
 }
